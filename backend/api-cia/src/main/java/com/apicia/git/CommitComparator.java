b21b0e0fefd36051912d660d47f4c546dbcd6295
Pragnesh Dubey <pragneshdubey20@gmail.com>
2026-08-02 14:10:19 +0530

    Use logger in CommitComparator

--- backend/api-cia/src/main/java/com/apicia/git/CommitComparator.java
+++ backend/api-cia/src/main/java/com/apicia/git/CommitComparator.java
@@ -1,15 +1,21 @@
 package com.apicia.git;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommitComparator {
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(CommitComparator.class);
+
     public void compareCommits(String before, String after) {
 
-        System.out.println("Comparing commits...");
-        System.out.println("Before : " + before);
-        System.out.println("After  : " + after);
+        logger.info("========== Comparing Commits ==========");
+        logger.info("Before Commit : {}", before);
+        logger.info("After Commit  : {}", after);
+        logger.info("=======================================");
 
     }
 }
